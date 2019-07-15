1da53571b07340f0af0782d53c51edf046b2892a
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-07-15 01:25:26 -0700

    Probot-53 Email validation and reset password

---
+++ controllers/email.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  name: 'www.probotplayground.com',
+  host: 'mail.probotplayground.com',
+  port: 26,
+  secure: false, // true for 465, false for other ports
+  ignoreTLS: true,
+  auth: {
+    user: 'donotreply@probotplayground.com',//process.env.EMAIL_USERNAME,
+    pass: 'proBot13!'//process.env.EMAIL_PASSWORD
+  }
+});
+
+sendAccountVerification = (link, email) => {
+  transporter.sendMail({
+    from: '"Probot Playground" <donotreply@probotplayground.com>', // sender address
+    to: email, // list of receivers
+    subject: 'Account Email Validation', // Subject line
+    text: `Thank you for creating a Probot Playground account. Please validate your email with the following link: ${link}`, // plain text body
+    html: `<p>Thank you for creating a Probot Playground account. Please validate your email with the following link: <a href='${link}'>Verify Email</a></p>` // html body
+  }, (err) => {
+    if (err)
+      console.log(err);
+  });
+};
+
+sendResetPassword = (link, email) => {
+  transporter.sendMail({
+    from: '"Probot Playground" <donotreply@probotplayground.com>', // sender address
+    to: email, // list of receivers
+    subject: 'Reset Password', // Subject line
+    text: `Please use the following link to reset your password: ${link}`, // plain text body
+    html: `<p>Please use the following link to reset your password: <a href='${link}'>Reset Password</a></p>` // html body
+  }, (err) => {
+    if (err)
+      console.log(err);
+  });
+};
+
+module.exports = {
+  sendAccountVerification: sendAccountVerification,
+  sendResetPassword: sendResetPassword
+};
+
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
-const users = require('./users');
+const user = require('./user');
 const tournament = require('./tournament');
 const bot = require('./bot');
 const table = require('./table');
 const metric = require('./metric');
 
 router.use('/tournament', tournament);
-router.use('/user', users);
+router.use('/user', user);
 router.use('/bot', bot);
 router.use('/table', table);
 router.use('/metric', metric);
