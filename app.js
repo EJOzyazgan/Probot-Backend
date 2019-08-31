a1dbbbe8e281508e287f7365daf6aec4dd46fe7e
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-08-30 23:02:00 -0700

    migrations

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ require('./config/passport');
 // Start cron jobs
 require('./controllers/cron');
 
-app.use(function (req,res,next) {
-  res.setHeader("Access-Control-Allow-Origin",  "*");
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', "PUT, PATCH, GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -30,9 +30,9 @@ app.use(function (req,res,next) {
 });
 
 io.on('connection', (socket) => {
-  console.log("Connected to Socket!!"+ socket.id);
+  console.log("Connected to Socket!!" + socket.id);
 
-  socket.on('room', (room)=>{
+  socket.on('room', (room) => {
     socket.join(room);
   });
 });
@@ -51,18 +51,24 @@ app.use(bodyParser.json());
 app.use('/api', routeIndex);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// models.sequelize.sync({alter: true}).then(() => {
+if (process.env.RUN_MIGRATIONS === 'true') {
+  models.sequelize.sync({ alter: true }).then(() => {
+    http.listen(port, () => {
+      console.log(`Listening on port ${port}!`)
+    });
+  });
+} else {
   http.listen(port, () => {
     console.log(`Listening on port ${port}!`)
   });
-// });
+}
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
